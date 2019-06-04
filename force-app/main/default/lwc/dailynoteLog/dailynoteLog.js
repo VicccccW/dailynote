1b2f0be3f12c8d96fb0398f2532e600d2d37af9b
Victor Wang <victor.wang@open.edu.au>
2019-06-04 23:15:18 +1000

    record form commit

--- force-app/main/default/lwc/dailynoteLog/dailynoteLog.js
+++ force-app/main/default/lwc/dailynoteLog/dailynoteLog.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class DailynoteLog extends LightningElement {}
+import TYPE_FIELD from '@salesforce/schema/Worklog__c.Type__c';
+import DATE_FIELD from '@salesforce/schema/Worklog__c.Date__c';
+import SUMMARY_FIELD from '@salesforce/schema/Worklog__c.Summary__c';
+import DETAIL_FIELD from '@salesforce/schema/Worklog__c.Log_Detail__c';
+
+export default class RecordFormCreate extends LightningElement {
+    // objectApiName is "Account" when this component is placed on an account record page
+    @api objectApiName;
+
+    fields = [TYPE_FIELD, DATE_FIELD, SUMMARY_FIELD, DETAIL_FIELD];
+
+    handleSuccess(event) {
+        const evt = new ShowToastEvent({
+            title: "Account created",
+            message: "Record ID: "+ event.detail.id,
+            variant: "success"
+        });
+        this.dispatchEvent(evt);
+    }
+}

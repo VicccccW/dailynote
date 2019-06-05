4e99411a651a45c6d4d579e9175bf1186274366c
Victor Wang <uniquewq91@gmail.com>
2019-06-05 11:29:49 +1000

    getListViewUi recent_worklog commit

---
+++ force-app/main/default/lwc/dailynoteLogRecentListView/dailynoteLogRecentListView.js
@@ -0,0 +1,28 @@
+import { LightningElement, wire, track } from 'lwc';
+import { getListUi} from 'lightning/uiListApi';
+import WORKLOG_OBJECT from '@salesforce/schema/Worklog__c';
+
+export default class DailynoteLogRecentListView extends LightningElement {
+    @track result;
+    @track error;
+
+    // @wire(getListUi, { objectApiName: WORKLOG_OBJECT, listViewApiName: 'Recent_Worklog' })
+    // listView;
+    
+    // get worklogs() {
+    //     return this.listView.data.records.records;
+    // }
+
+
+
+    @wire(getListUi, { objectApiName: WORKLOG_OBJECT, listViewApiName: 'Recent_Worklog' })
+    wiredlistView({ error, data }) {
+        if(data) {
+            this.result = data.records.records;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+
+}

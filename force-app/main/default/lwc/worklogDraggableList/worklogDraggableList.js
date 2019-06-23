5989be08027f7be42d2422530237abcac2957506
Victor Wang <victor.wang@open.edu.au>
2019-06-23 12:39:56 +1000

    enable and disable button

--- force-app/main/default/lwc/worklogDraggableList/worklogDraggableList.js
+++ force-app/main/default/lwc/worklogDraggableList/worklogDraggableList.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import { LightningElement, wire, track} from 'lwc';
-import { refreshApex } from '@salesforce/apex';
+//import { refreshApex } from '@salesforce/apex';
 import getAllWorklogs from '@salesforce/apex/WorklogController.getAllWorklogs';
 import saveWorklogs from '@salesforce/apex/WorklogController.saveWorklogs';
 import { CurrentPageReference } from 'lightning/navigation';
 import { fireEvent } from 'c/pubsub';
+import { reduceErrors } from 'c/idsUtils';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 
 export default class WorklogDraggableList extends LightningElement {
@@ -46,15 +48,26 @@ export default class WorklogDraggableList extends LightningElement {
 
         remainingItems.splice(event.detail.newIndex, 0, movedItem);
         
+        //change index
         remainingItems.forEach((element, index) => {
             element.Drag_Table_Index__c = index;
         });
 
+        //re-order new items based on new index
         remainingItems.sort((a, b) => {
             return a.Drag_Table_Index__c - b.Drag_Table_Index__c
         });
 
+        //update worklogs property
         this.worklogs = remainingItems;
+
+        //enable buttons 
+        this.template.querySelectorAll('button').forEach(element => {
+            element.removeAttribute("disabled");
+        });
+
+        // const test = this.template.querySelectoryAll('.button');
+        // console.log(test);
     }
 
     handleSelect(event) {
@@ -62,21 +75,50 @@ export default class WorklogDraggableList extends LightningElement {
     }
 
     handleRevert(event) {
+        //disable button
+        this.template.querySelectorAll('button').forEach(element => {
+            element.setAttribute("disabled", null);
+        });
+
+
         this.worklogs = this._originalArr;
     }
 
-    handleSave(event) {        
+    handleSave(event) {  
+        //disable button
+        this.template.querySelectorAll('button').forEach(element => {
+            element.setAttribute("disabled", null);
+        });
+        
+
+        //save data to server
         const savedWorklogsStr = JSON.stringify(this.worklogs);
 
         saveWorklogs({ savedWorklogsStr : savedWorklogsStr })
             .then(() => {
                 this._originalArr = this.worklogs;
                 //return refreshApex(this.wiredWorklogs);
+
                 //dispatch success message 
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Index Updated',
+                        variant: 'success'
+                    })
+                );
             })
             .catch(error => {
                 this.error = error;
-                //display error message
+
+                //dispatch error message
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating record',
+                        message: reduceErrors(error).join(', '),
+                        variant: 'error'
+                    })
+                );
             })
     }
 }
